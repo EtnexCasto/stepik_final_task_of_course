184a9f236863cbaa89b2f05b01fa746c5ff65681
EtnexCasto <bro04280@gmail.com>
2025-05-06 12:12:24 +0500

    Изменено количество входных параметров в методе поиска элемента

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
@@ -8,4 +7,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert BasePage.is_element_present(self, *MainPageLocators.LOGIN_LINK), "Login link is not presented"
