ab0bc83597f2d8efcacbd2b0525adf1a56514216
EtnexCasto <bro04280@gmail.com>
2025-05-06 12:14:43 +0500

    Метод поиска элемента теперь возвращает истину в при успехе

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,6 +14,5 @@ class BasePage():
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
-        return False
-            
+        return True
         
