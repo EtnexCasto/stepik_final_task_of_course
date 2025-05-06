42590ee16326bed4f02ddb25a052533ae46c2730
EtnexCasto <bro04280@gmail.com>
2025-05-06 14:22:17 +0500

    Добавлен класс с константами для страницы продукта

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    STRONG_NAME_PRODUCT = (By.CSS_SELECTOR, "div.alertinner strong")
+    STRONG_PRICE_PRODUCT = (By.CSS_SELECTOR, "div.alertinner p strong")
