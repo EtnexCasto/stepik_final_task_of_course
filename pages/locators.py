7fe9a062829a65952013ca4ca12730ec1499da4c
EtnexCasto <bro04280@gmail.com>
2025-05-12 15:16:39 +0500

    Добавил локаторы для формы регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,11 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+    EMAIL_INPUT = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD_AGAIN_INPUT = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_SUBMIT_BUTTON = (By.NAME, "registration_submit")
+
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     NAME_PRODUCT = (By.CSS_SELECTOR, "div.product_main h1")
@@ -23,3 +28,4 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     GO_BASKET = (By.CSS_SELECTOR, "span.btn-group a.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
