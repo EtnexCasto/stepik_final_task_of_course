6fbefac5d9bef22d49984ce3a584a190d7b8e7c6
EtnexCasto <bro04280@gmail.com>
2025-05-12 15:14:39 +0500

    Добавил метод регистрации пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -17,3 +17,13 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Форма регистрации не найдена."
+
+    def register_new_user(self, email, password):
+        email_find = self.browser.find_element(*LoginPageLocators.EMAIL_INPUT)
+        email_find.send_keys(email)
+        password1_input = self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT)
+        password1_input.send_keys(password)
+        password2_input = self.browser.find_element(*LoginPageLocators.PASSWORD_AGAIN_INPUT)
+        password2_input.send_keys(password)
+        reg_button = self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT_BUTTON)
+        reg_button.click()
