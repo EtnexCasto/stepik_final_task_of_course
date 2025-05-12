926f4755c4def392bd5c240acada226cbcd4c04f
EtnexCasto <bro04280@gmail.com>
2025-05-12 15:17:37 +0500

    Добавил класс теста корзины + проверка сетапа

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,38 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+
+
+@pytest.mark.actual
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_url = browser.current_url
+        login_page = LoginPage(browser, login_url)
+
+        email = str(time.time()) + "@fakemail.org"
+        login_page.register_new_user(email, password="12341231249109419")
+        login_page.should_be_authorized_user()
+
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_basket_button()
+        page.add_to_basket()
+
+
 
 @pytest.mark.neactual
 def test_guest_can_add_product_to_basket(browser):
@@ -38,14 +69,14 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.should_be_disappeared_success_message()
 
-@pytest.mark.actual
+@pytest.mark.neactual
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.actual
+@pytest.mark.neactual
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
