5f1edc46e470799450b1a301da673793e6f2939a
EtnexCasto <bro04280@gmail.com>
2025-05-12 22:43:54 +0500

    Добавли недостающий тест корзины и маркировку для ревью

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import pytest
 import time
 
@@ -24,13 +25,14 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
         page.should_be_basket_button()
         page.add_to_basket()
 
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link1 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link1)
@@ -71,8 +73,21 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = ProductPage(browser,  link)
+        page.open()
+
+        page.go_to_basket()
+        basket_url = browser.current_url
+        basket_page = BasketPage(browser, basket_url)
+        basket_page.should_not_be_basket_form()
+        basket_page.should_be_message_basket_empty()
