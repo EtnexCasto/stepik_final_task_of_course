b15fe0db753b0343b1dd2f53a0e6570563fa19db
EtnexCasto <bro04280@gmail.com>
2025-05-12 12:15:14 +0500

    Добавлены тесты на отсутствие сообщения о добавлении товара в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,10 @@
 from pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser, link):
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+
+@pytest.mark.notactual
+def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
 
@@ -8,3 +12,28 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_equal_name_product_and_basket()
+
+@pytest.mark.actual
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+
+    page.should_be_basket_button()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+@pytest.mark.actual
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+
+    page.should_not_be_success_message()
+    
+@pytest.mark.actual
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+
+    page.should_be_basket_button()
+    page.add_to_basket()
+    page.should_be_disappeared_success_message()
