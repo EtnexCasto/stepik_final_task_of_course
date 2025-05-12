eca02f7ecc08e9e965edbe8b4ed79c8627f79c7d
EtnexCasto <bro04280@gmail.com>
2025-05-12 13:58:13 +0500

    Плюс тест проверки корзины на пустоту и соответствующем сообщении

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
+import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
@@ -11,3 +13,15 @@ def test_guest_can_go_to_login_page(browser):
     login_url = browser.current_url
     login_page = LoginPage(browser, login_url)
     login_page.should_be_login_page()
+
+@pytest.mark.actual
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser,  link)
+    page.open()
+
+    page.go_to_basket()
+    basket_url = browser.current_url
+    basket_page = BasketPage(browser, basket_url)
+    basket_page.should_not_be_basket_form()
+    basket_page.should_be_message_basket_empty()
